04e407c932ab7871979a3898f5f4d7e60810aa65
Ralph van Schaik <ralph.van.schaik@me.com>
2020-11-14 14:20:22 +0100

    Initial commit

---
+++ assignment.js
@@ -0,0 +1,15 @@
+const li = document.querySelector('li');
+// console.dir(h1);
+// li.style.backgroundColor = 'black';
+// li.style.color = 'white';
+
+const lid = document.getElementById('task-1');
+lid.style.backgroundColor = 'grey';
+lid.style.color = 'white';
+
+const headText = document.querySelector('head title');
+console.dir(headText);
+headText.textContent = 'Assignment - Solved!';
+
+const h1 = document.querySelector('h1');
+h1.textContent = 'Assignment - Solved!';
